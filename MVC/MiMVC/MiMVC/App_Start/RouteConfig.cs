b005d9884e4fec064722fcdb58d5ed4615b75e9c
BruS025 <ielbrui@gmail.com>
2017-12-21 15:25:34 -0300

    MVC y HTML5

---
+++ MVC/MiMVC/MiMVC/App_Start/RouteConfig.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace MiMVC
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                "Procesos",
+                "Procesos/{action}/{id}",
+                defaults : new { Controller = "Procesos", action = "Listado", id = UrlParameter.Optional }
+                );
+
+            routes.MapRoute(//Donde viene definida la url por defecto
+                name: "Default",//nombre por defecto
+                url: "{controller}/{action}/{id}",//forma de la url por defecto
+                defaults: new { controller = "Home", action = "Prueba", id = UrlParameter.Optional }//objeto de instacia de ruta
+            );
+        }
+    }
+}
---
+++ MVC/MiMVC/MiMVC/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MiMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public string Index()//metodo que va en url por defecto 
+        {
+            return "Mi app MVC 5";
+        }
+
+
+    }
+}
